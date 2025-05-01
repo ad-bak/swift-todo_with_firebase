41c74a74421b8d60d39bf4c135abd2b6a3ab1217
adbak <adbak.dev@gmail.com>
2025-05-01 18:52:24 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Adnan Bakaev  on 01/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
